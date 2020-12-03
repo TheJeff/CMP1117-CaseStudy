84f1979a20c14ff6fe78a179c120ef5a45315482
Jeff Nicholls <TheJeff@users.noreply.github.com>
2020-12-03 00:44:57 -0500

    Add TODOs

---
+++ main.py
@@ -0,0 +1,43 @@
+import pickle
+
+def main():
+    #TODO MENU 
+
+    #TODO LOAD Glossary (read method) and make a pickle of it
+
+    #TODO Load Team file Format into dictionaries, pickle it
+
+    #TODO Player Average Function
+
+    #TODO Team Average Function
+
+    #TODO Print Team List
+
+    #TODO Print Rank Team members
+
+def getPlayerAverage(player):
+    
+    #TODO
+
+    return average
+
+def getTeamAverage(team_dict):
+
+    #TODO
+
+    return average 
+
+def printTeam(team_dict):
+
+    #TODO
+
+    return 0
+
+def exportTeam(team_dict)
+
+    #TODO export team to txt file formatted
+
+    return 0
+
+
+
